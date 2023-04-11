97f3a2618e1d004780b90628693804721b4413a4
Albina <latipova.alb@gmail.com>
2023-04-11 16:44:53 +0500

    order card

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,32 +1,31 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class CardTest {
-    private String generateData(int addDays) {
-        return LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
 
     @Test
-    public void orderFormTest() {
+    void shouldSendForm() {
+        Configuration.headless = true;
+        Configuration.holdBrowserOpen = true;
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Уфа");
-        String currentDate = generateData(7);
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='date'] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(date);
         $("[data-test-id='name'] input").setValue("Долгая Аля");
         $("[data-test-id='phone'] input").setValue("+74457786643");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+        $("[data-test-id=agreement]").click();
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.ownText(date));
     }
 }
