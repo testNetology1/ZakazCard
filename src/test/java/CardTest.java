9461487619065f9351ce8504ad331a811980c392
Albina <latipova.alb@gmail.com>
2023-04-26 21:05:40 +0500

    card commit2

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,6 +1,4 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -8,38 +6,29 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class CardTest {
-    @BeforeEach
-    public void setup() {
+
+    @Test
+    public void DeliveryCard() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-    }
-
-    public String generateDate(long days, String ignoredPattern) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    @Test
-    void shouldSendForm() {
-        $(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $("[data-test-id='city'] input").setValue("Уфа");
-        $("[data-test-id='date'] input").doubleClick();
-        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-        String date = generateDate( 3,"dd.MM.yyyy");
-        $("[data-test-id='date'] input").setValue(date);
-        $("[data-test-id='name'] input").setValue("Долгая Аля Карловна");
-        $("[data-test-id='phone'] input").setValue("+79123456789");
+        $("[data-test-id=city] input").setValue("Уфа");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now().plusDays(4)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Долгая Аля Карловна");
+        $("[data-test-id=phone] input").setValue("+79123456789");
         $("[data-test-id=agreement]").click();
-        $$("button").find(Condition.exactText("Забронировать")).click();
-       // $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на  + planningDate)"
-               ), Duration.ofSeconds(15))
-                .shouldBe(Condition.visible);
-        ///$(".notification__content").shouldHave(Condition.ownText(date));
+        $(".button").shouldHave(text("Забронировать")).click();
+        $("[data-test-id=notification]")
+                .shouldHave(text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));
     }
 }
 
